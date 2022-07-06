3f3fb18a268d5f0cb530f1fc987738f95d7f660b
Arxaid <VallravenWasTaken@gmail.com>
2022-07-06 13:24:48 +0300

    Work in progress

--- src/motodriver.h
+++ src/motodriver.h
@@ -9,26 +9,7 @@
 #include <ArduinoSTL.h>
 #include <map>
 #include <string>
-
-/**
-* @brief Supporting Drivers:    
-* L298N 2X Motor Shield, 
-* Other.
-*/
-typedef enum {
-    L298N = 1, 
-    OTHER = 0
-} ShieldDriversNames;
-
-/**
- * @brief Supporting directions: 
- * Forward, Backward, Shutdown
- */
-typedef enum {
-    FORWARD = 0, 
-    BACKWARD = 1,
-    SHUTDOWN = -1
-} MoveDirection;
+#include "type-declaration.h"
 
 class Motor{
     private:
@@ -37,7 +18,6 @@ class Motor{
         int8_t direction_pin2;
         int8_t dcycle;
         int8_t direct;
-    
     public:
         int8_t motorID;
         int8_t shield_driver_type;
@@ -55,12 +35,10 @@ class Motor{
         * @param pwm_pin PWM (Pulse-width modulation) controller pin number.
         * @param direction_pin1 First rotating direction pin number.
         * @param direction_pin2 Second rotating direction pin number(if exists).
-        * @param dcycle Duty cycle value.
-        * @param direct Direction value.
         * @param shield_driver_name Name of your motor driver shield (check for support).
         */
-        Motor(const int8_t &_pwm_pin, const int8_t &_direction_pin1, const int8_t &_direction_pin2, 
-              const int8_t &_dcycle, const int8_t &_direct, const std::string &_shield_driver_name);
+        Motor(const std::string &_shield_driver_name, const int8_t &_pwm_pin, 
+              const int8_t &_direction_pin1, const int8_t &_direction_pin2);
 
         /**
         * @brief Motor object destructor by default.
@@ -126,139 +104,6 @@ class Motor{
 
 };
 
-class MotorX2{
-    private:
-        int8_t dcycleA;
-        int8_t dcycleB;
-        int8_t direct;
-    public:
-        Motor motorA;
-        Motor motorB;
-        int8_t motorID;
-        int8_t shield_driver_type_A;
-        int8_t shield_driver_type_B;
-        std::string shield_driver_name_A;
-        std::string shield_driver_name_B;
-
-        #pragma region Initialization
-
-        /**
-        * @brief MotorX2 object constructor by default.
-        */
-        MotorX2(); 
-
-        /**
-        * @brief MotorX2 object constructor.
-        * @param pwm_pin_A [First] PWM (Pulse-width modulation) controller pin number.
-        * @param pwm_pin_B [Second] PWM (Pulse-width modulation) controller pin number.
-        * @param direction_pin1_A [First] First rotating direction pin number.
-        * @param direction_pin2_A [First] Second rotating direction pin number(if exists).
-        * @param direction_pin1_B [Second] First rotating direction pin number.
-        * @param direction_pin2_B [Second] Second rotating direction pin number(if exists).
-        * @param dcycle_A [First] Duty cycle value.
-        * @param dcycle_B [Second] Duty cycle value.
-        * @param direct [Both] Direction value.
-        * @param shield_driver_name_A [First] Name of your motor driver shield (check for support).
-        * @param shield_driver_name_B [Second] Name of your motor driver shield (check for support).
-        */
-        MotorX2(const int8_t &_pwm_pin_A, const int8_t &_pwm_pin_B, 
-                const int8_t &_direction_pin1_A, const int8_t &_direction_pin2_A, 
-                const int8_t &_direction_pin1_B, const int8_t &_direction_pin2_B, 
-                const int8_t &_dcycle_A, const int8_t &_dcycle_B, 
-                const int8_t &_direct,
-                const std::string &_shield_driver_name_A, const std::string &_shield_driver_name_B);
-    
-        /**
-        * @brief Motor object destructor by default.
-        */
-        ~MotorX2();        
-
-        #pragma endregion
-        #pragma region Accessors
-
-        /**
-        * @brief [Both] Set new direction.
-        */
-        void set_direct(int8_t _direct);
-
-        /**
-        * @brief [First] Set new duty cycle.
-        */
-        void set_dcycle_A(int8_t _dcycle_A);
-
-        /**
-        * @brief [Second] Set new duty cycle.
-        */
-        void set_dcycle_B(int8_t _dcycle_B);
-
-        /**
-        * @brief [Both] Get current direction.
-        */
-        int8_t get_direct();
-
-        /**
-        * @brief [First] Get current duty cycle.
-        */
-        int8_t get_dcycle_A();
-
-        /**
-        * @brief [Second] Get current duty cycle.
-        */
-        int8_t get_dcycle_B();
-
-        /**
-        * @brief [Both] Get current direction by link.
-        */
-        int8_t &direction_() {
-            return direct;
-        }
-
-        /**
-        * @brief [First] Get current duty cycle by link.
-        */
-        int8_t &duty_cycle_A_() {
-            return dcycleA;
-        }
-
-        /**
-        * @brief [Second] Get current duty cycle by link.
-        */
-        int8_t &duty_cycle_B_() {
-            return dcycleB;
-        }
-
-        #pragma endregion
-        #pragma region Functions
-
-        /**
-        * @result [Both] Shield driver names.
-        */        
-        void print8_t_driver_names();
-
-        /**
-        * @brief [First] Move with set direction and duty cycle.
-        */
-        void RotateA();
-
-        /**
-        * @brief [Second] Move with set direction and duty cycle.
-        */
-        void RotateB();
-
-        /**
-        * @brief [First] Stop moving.
-        */
-        void ShutdownA();
-
-        /**
-        * @brief [Second] Stop moving.
-        */
-        void ShutdownB();
-
-        #pragma endregion
-
-};
-
 class MotoDriver{
     private:
         std::map<int8_t, Motor> motors;
@@ -304,95 +149,6 @@ class MotoDriver{
         * @param motorID Current motor you want to stop.
         */
         void Shutdown(const int8_t &motorId);
-
-        #pragma endregion
-};
-
-class MotoDriverX2{
-    private:
-        std::map<int8_t, MotorX2> motors;
-        boolean isMoving;
-    public:
-        #pragma region Initialization
-
-        /**
-        * @brief MotoDriver object constructor by default.
-        */
-        MotoDriverX2();
-
-        /**
-        * @brief MotoDriverX2 object constructor.
-        * @param motor Motor instance.
-        */
-        MotoDriverX2(std::map<int8_t, MotorX2> &_motors);
-
-        /**
-        * @brief MotoDriverX2 object destructor by default.
-        */
-        ~MotoDriverX2();
-
-        #pragma endregion
-        #pragma region Functions
-
-        /**
-        * @brief [First] Move backward with set duty cycle if pwm pin exists, if not - move backward with 255.
-        * @param dcycle Duty cycle value.
-        * @param motorID Current motor you want to rotate.
-        */
-        void BackwardA(const int8_t &_dcycleA, const int8_t &motorId);
-
-        /**
-        * @brief [Second] Move backward with set duty cycle if pwm pin exists, if not - move backward with 255.
-        * @param dcycle Duty cycle value.
-        * @param motorID Current motor you want to rotate.
-        */
-        void BackwardB(const int8_t &_dcycleA, const int8_t &motorId);
-
-        /**
-        * @brief [Both] Move backward with set duty cycle if pwm pin exists, if not - move backward with 255.
-        * @param dcycle Duty cycle value.
-        * @param motorID Current motor you want to rotate.
-        */
-        void BackwardBoth(const int8_t &_dcycleA, const int8_t &_dcycleB, const int8_t &motorId);
-
-        /**
-        * @brief [First] Move forward with set duty cycle if pwm pin exists, if not - move forward with 255.
-        * @param dcycle Duty cycle value.
-        * @param motorID Current motor you want to rotate.
-        */
-        void ForwardA(const int8_t &_dcycleA, const int8_t &motorId);
-
-        /**
-        * @brief [Second] Move forward with set duty cycle if pwm pin exists, if not - move forward with 255.
-        * @param dcycle Duty cycle value.
-        * @param motorID Current motor you want to rotate.
-        */
-        void ForwardB(const int8_t &_dcycleA, const int8_t &motorId);
-
-        /**
-        * @brief [Both] Move forward with set duty cycle if pwm pin exists, if not - move forward with 255.
-        * @param dcycle Duty cycle value.
-        * @param motorID Current motor you want to rotate.
-        */
-        void ForwardBoth(const int8_t &_dcycleA, const int8_t &_dcycleB, const int8_t &motorId);
-
-        /**
-        * @brief [First] Stop moving.
-        * @param motorID Current motor you want to stop.
-        */
-        void ShutdownA(const int8_t &motorId);
-
-        /**
-        * @brief [Second] Stop moving.
-        * @param motorID Current motor you want to stop.
-        */
-        void ShutdownB(const int8_t &motorId);
-
-        /**
-        * @brief [Both] Stop moving.
-        * @param motorID Current motor you want to stop.
-        */
-        void ShutdownBoth(const int8_t &motorId);
 
         #pragma endregion
 };
--- src/motordriver.cpp
+++ src/motordriver.cpp
@@ -6,12 +6,12 @@
 
 #include "motodriver.h"
 
-#pragma region Motor
+#pragma region Motor Initialization
 
 Motor::Motor() = default;
 
-Motor::Motor(const int8_t &_pwm_pin, const int8_t &_direction_pin1, const int8_t &_direction_pin2,
-             const int8_t &_dcycle, const int8_t &_direct, const std::string &_shield_driver_name){
+Motor::Motor(const std::string &_shield_driver_name, const int8_t &_pwm_pin, 
+             const int8_t &_direction_pin1, const int8_t &_direction_pin2){
        
     if(_shield_driver_name == "L298N"){
 
@@ -19,7 +19,7 @@ Motor::Motor(const int8_t &_pwm_pin, const int8_t &_direction_pin1, const int8_t
         pwm_pin = _pwm_pin;
         direction_pin1 = _direction_pin1;
         direction_pin2 = _direction_pin2;
-        dcycle = _dcycle;
+        dcycle = 100;
         direct = MoveDirection::SHUTDOWN;
 
         pinMode(pwm_pin, OUTPUT);
@@ -32,7 +32,7 @@ Motor::Motor(const int8_t &_pwm_pin, const int8_t &_direction_pin1, const int8_t
         shield_driver_type = (int8_t)ShieldDriversNames::OTHER;
         direction_pin1 = _direction_pin1;
         direction_pin2 = _direction_pin2;
-        dcycle = 255;
+        dcycle = 100;
         direct = MoveDirection::SHUTDOWN;
 
         pinMode(direction_pin1, OUTPUT);
@@ -42,103 +42,39 @@ Motor::Motor(const int8_t &_pwm_pin, const int8_t &_direction_pin1, const int8_t
 
 Motor::~Motor(){}
 
+#pragma endregion;
+#pragma region Motor Accessors
+
 void Motor::set_direct(int8_t _direct){
     direct = _direct;
 }
-
 void Motor::set_dcycle(int8_t _dcycle){
     dcycle = _dcycle;
 }
-
 int8_t Motor::get_direct(){
     return direct;
 }
-
 int8_t Motor::get_dcycle(){
     return dcycle;
 }
 
+#pragma endregion
+#pragma region Motor Functions
+
 void Motor::print8_t_driver_name(){
     std::cout << "Motor driver name is " << shield_driver_name << "\n";
 }
-
 void Motor::Rotate(){
     digitalWrite(direction_pin1, direct);
     analogWrite(pwm_pin, dcycle);  
 }
-
 void Motor::Shutdown(){
-    digitalWrite(direction_pin1, LOW);
-    digitalWrite(direction_pin2, LOW);
+    digitalWrite(direction_pin1, MoveDirection::SHUTDOWN);
     analogWrite(pwm_pin, dcycle);
 }
 
 #pragma endregion
-#pragma region MotorX2
-
-MotorX2::MotorX2() = default;
-
-MotorX2::MotorX2(const int8_t &_pwm_pin_A, const int8_t &_pwm_pin_B,
-                 const int8_t &_direction_pin1_A, const int8_t &_direction_pin2_A,
-                 const int8_t &_direction_pin1_B, const int8_t &_direction_pin2_B,
-                 const int8_t &_dcycle_A, const int8_t &_dcycle_B,
-                 const int8_t &_direct,
-                 const std::string &_shield_driver_name_A, const std::string &_shield_driver_name_B):
-                 motorA(_pwm_pin_A, _direction_pin1_A, _direction_pin2_A, _dcycle_A,
-                 _direct, _shield_driver_name_A), 
-                 motorB(_pwm_pin_B, _direction_pin1_B, _direction_pin2_B, _dcycle_B,
-                 _direct, _shield_driver_name_B){}
-
-MotorX2::~MotorX2(){}
-
-void MotorX2::set_direct(int8_t _direct){
-    direct = _direct;
-}
-
-void MotorX2::set_dcycle_A(int8_t _dcycle_A){
-    dcycleA = _dcycle_A;
-}
-
-void MotorX2::set_dcycle_B(int8_t _dcycle_B){
-    dcycleB = _dcycle_B;
-}
-
-int8_t MotorX2::get_direct(){
-    return direct;
-}
-
-int8_t MotorX2::get_dcycle_A(){
-    return dcycleA;
-}
-
-int8_t MotorX2::get_dcycle_B(){
-    return dcycleB;
-}
-
-void MotorX2::print8_t_driver_names(){
-    std::string outputData = "First motor driver name is " + shield_driver_name_A + "\n" + 
-                             "Second motor driver name is " + shield_driver_name_B + "\n";
-    std::cout << outputData; 
-}
-
-void MotorX2::RotateA(){
-    motorA.Rotate();
-}
-
-void MotorX2::RotateB(){
-    motorB.Rotate();
-}
-
-void MotorX2::ShutdownA(){
-    motorA.Shutdown();
-}
-
-void MotorX2::ShutdownB(){
-    motorB.Shutdown();
-}
-
-#pragma endregion
-#pragma region MotoDriver
+#pragma region MotoDriver Initialization
 
 MotoDriver::MotoDriver() = default; 
 
@@ -148,12 +84,15 @@ MotoDriver::MotoDriver(std::map<int8_t, Motor> &_motors) {
 
 MotoDriver::~MotoDriver(){}
 
+#pragma endregion
+#pragma region MotoDriver Functions
+
 void MotoDriver::Backward(const int8_t &_dcycle, const int8_t &motorId){     
     motors[motorId].direction_() = MoveDirection::BACKWARD;
     motors[motorId].duty_cycle_() = _dcycle;
     motors[motorId].Rotate();  
 
-    isMoving = true; 
+    isMoving = true;
 }
 
 void MotoDriver::Forward(const int8_t &_dcycle, const int8_t &motorId){     
@@ -168,91 +107,6 @@ void MotoDriver::Shutdown(const int8_t &motorId){
     motors[motorId].direction_() = MoveDirection::SHUTDOWN;
     motors[motorId].Shutdown();  
     
-    isMoving = false; 
-}
-
-#pragma endregion
-#pragma region MotoDriverX2
-
-MotoDriverX2::MotoDriverX2() = default; 
-
-MotoDriverX2::MotoDriverX2(std::map<int8_t, MotorX2> &_motors) {
-    motors = _motors;
-}
-
-MotoDriverX2::~MotoDriverX2(){}
-
-void MotoDriverX2::BackwardA(const int8_t &_dcycleA, const int8_t &motorId){     
-    motors[motorId].direction_() = MoveDirection::BACKWARD;
-    motors[motorId].duty_cycle_A_() = _dcycleA;
-    motors[motorId].RotateA();  
-
-    isMoving = true; 
-}
-
-void MotoDriverX2::BackwardB(const int8_t &_dcycleB, const int8_t &motorId){     
-    motors[motorId].direction_() = MoveDirection::BACKWARD;
-    motors[motorId].duty_cycle_B_() = _dcycleB;
-    motors[motorId].RotateB();  
-
-    isMoving = true; 
-}
-
-void MotoDriverX2::BackwardBoth(const int8_t &_dcycleA, const int8_t &_dcycleB, const int8_t &motorId){     
-    motors[motorId].direction_() = MoveDirection::BACKWARD;
-    motors[motorId].duty_cycle_A_() = _dcycleA;
-    motors[motorId].RotateA(); 
-    motors[motorId].duty_cycle_B_() = _dcycleB;
-    motors[motorId].RotateB();  
-
-    isMoving = true; 
-}
-
-void MotoDriverX2::ForwardA(const int8_t &_dcycleA, const int8_t &motorId){     
-    motors[motorId].direction_() = MoveDirection::FORWARD;
-    motors[motorId].duty_cycle_A_() = _dcycleA;
-    motors[motorId].RotateA();  
-
-    isMoving = true; 
-}
-
-void MotoDriverX2::ForwardB(const int8_t &_dcycleB, const int8_t &motorId){     
-    motors[motorId].direction_() = MoveDirection::FORWARD;
-    motors[motorId].duty_cycle_B_() = _dcycleB;
-    motors[motorId].RotateB();  
-
-    isMoving = true; 
-}
-
-void MotoDriverX2::ForwardBoth(const int8_t &_dcycleA, const int8_t &_dcycleB, const int8_t &motorId){     
-    motors[motorId].direction_() = MoveDirection::FORWARD;
-    motors[motorId].duty_cycle_A_() = _dcycleA;
-    motors[motorId].RotateA(); 
-    motors[motorId].duty_cycle_B_() = _dcycleB;
-    motors[motorId].RotateB();  
-
-    isMoving = true; 
-}
-
-void MotoDriverX2::ShutdownA(const int8_t &motorId){     
-    motors[motorId].direction_() = MoveDirection::SHUTDOWN;
-    motors[motorId].ShutdownA();  
-
-    isMoving = false; 
-}
-
-void MotoDriverX2::ShutdownB(const int8_t &motorId){     
-    motors[motorId].direction_() = MoveDirection::SHUTDOWN;
-    motors[motorId].ShutdownB();  
-
-    isMoving = false; 
-}
-
-void MotoDriverX2::ShutdownBoth(const int8_t &motorId){     
-    motors[motorId].direction_() = MoveDirection::SHUTDOWN;
-    motors[motorId].ShutdownA(); 
-    motors[motorId].ShutdownB();  
-
     isMoving = false; 
 }
 
