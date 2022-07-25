2b20a90d4137d95672435322ed46c42ac5cd5f97
WDA <engineer13@outlook.com>
2022-07-25 21:05:04 +0300

    main

--- examples/motodriver.Ex04.ino/motodriver.Ex04.ino.ino
+++ examples/motodriver.Ex04.ino/motodriver.Ex04.ino.ino
@@ -1,7 +1,5 @@
 // This file is part of the Motodriver project.
 //
-// Copyright (c) 2022 Institute of Control Sciences RAS.
-//
 // Feel free to use, copy, modify, merge, and publish this software.
 
 #include <avr/io.h>
